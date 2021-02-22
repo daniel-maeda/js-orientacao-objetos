7ed5354a95b0cd540f643ec3901e8c59eb703b6b
Daniel F. Maeda <68878528+daniel-maeda@users.noreply.github.com>
2021-02-22 20:28:49 -0300

    using readline-sync

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
+import { question } from "readline-sync";
 import {Cliente} from "./Cliente.js"
 import {ContaCorrente} from "./ContaCorrente.js"
 
-const cliente1 = new Cliente("Ricardo", 11122233309);
-const cliente2 = new Cliente("Alice", 88822233309);
+const clientes = new Array(3);
+// clientes.forEach(cliente =>{
+//     let nome = question("Qual e' o seu nome? ");
+//     let cpf = question("Qual e' o seu cpf? ");
+//     cliente = new Cliente(nome, cpf);
+//     console.log(cliente);
+// });
 
 
-const contaCorrenteRicardo = new ContaCorrente(1001, cliente1);
-contaCorrenteRicardo.depositar(500);
-contaCorrenteRicardo.sacar(100);
+for (let i = 0; i < clientes.length; i++) {
+        let nome = question("Qual e' o seu nome? ");
+        let cpf = question("Qual e' o seu cpf? ");
+        clientes[i] = new Cliente(nome, cpf);
+        console.log(clientes[i]);
+}
+    
+console.log(clientes);
 
-const conta2 = new ContaCorrente(102, cliente2);
 
-let valor = 200;
-contaCorrenteRicardo.tranferir(valor, conta2);
+//const cliente2 = new Cliente("Alice", 88822233309);
 
-console.log(contaCorrenteRicardo);
+
+// const contaCorrenteRicardo = new ContaCorrente(1001, cliente1);
+// contaCorrenteRicardo.depositar(500);
+// contaCorrenteRicardo.sacar(100);
+
+// const conta2 = new ContaCorrente(102, cliente2);
+
+// let valor = 200;
+// contaCorrenteRicardo.tranferir(valor, conta2);
+
+// console.log(contaCorrenteRicardo);
